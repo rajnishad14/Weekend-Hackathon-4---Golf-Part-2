13601fd67af13336f1d23837a6c0da869e2d5021
Raj Kumar Nishad <raj.nishad14@gmail.com>
2021-04-01 00:17:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ import "../styles/App.css";
 
 const App = () => {
   const [renderBall, setRenderBall] = useState(false);
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  // const [x, setX] = useState(0);
+  // const [y, setY] = useState(0);
   const [ballPosition, setBallPosition] = useState({
-    left: "0px",
-    top: "0px",
+    left: 0,
+    top: 0,
   });
   const reset = () => {
     setRenderBall(false);
@@ -23,20 +23,20 @@ const App = () => {
   const move = (e) => {
     switch (e.keyCode) {
       case 37:
-        setX(x - 5);
-        setBallPosition({ ...ballPosition, left: `${x}px` });
+        // setX(x - 5);
+        setBallPosition({ ...ballPosition, left: ballPosition.left - 5 });
         break;
       case 38:
-        setY(y - 5);
-        setBallPosition({ ...ballPosition, top: `${y}px` });
+        // setY(y - 5);
+        setBallPosition({ ...ballPosition, top: ballPosition.top - 5 });
         break;
       case 39:
-        setX(x + 5);
-        setBallPosition({ ...ballPosition, left: `${x}px` });
+        // setX(x + 5);
+        setBallPosition({ ...ballPosition, left: ballPosition.left + 5 });
         break;
       case 40:
-        setY(y + 5);
-        setBallPosition({ ...ballPosition, top: `${y}px` });
+        // setY(y + 5);
+        setBallPosition({ ...ballPosition, top: ballPosition.top + 5 });
         break;
     }
   }
@@ -53,8 +53,8 @@ const App = () => {
       {
         renderBall ? <div className="ball" style={{
           position: "absolute",
-          top: `${ballPosition.top}`,
-          left: `${ballPosition.left}`
+          top: `${ballPosition.top}px`,
+          left: `${ballPosition.left}px`
         }}>
 
         </div> : <button onClick={start} className="start">Start</button>
